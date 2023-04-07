c48e2a57c4b20603d1630e802bc59323f3fc3a5e
nwahameed <nwahameed@gmail.com>
2023-04-07 16:53:01 +0300

    Create Components, pages and assets folders

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,73 @@
+import React, { useRef } from "react";
+import { Container } from "reactstrap";
+import "./navbar.css";
+import logo from "../../assets/logo.png";
+
+const navLinks = [
+  {
+    display: "Home",
+    url: "#",
+  },
+  {
+    display: "About",
+    url: "#",
+  },
+
+  {
+    display: "Courses",
+    url: "#",
+  },
+  {
+    display: "Kids",
+    url: "#",
+  },
+  {
+    display: "Contact",
+    url: "#",
+  },
+];
+
+const Navbar = () => {
+  const menuRef = useRef();
+
+  const menuToggle = () => menuRef.current.classList.toggle("active__menu");
+
+  return (
+    <header className="header">
+      <Container>
+        <div className="navigation d-flex align-items-center justify-content-between">
+          <div class="logo">
+            <img src={logo} alt="" className="logo" />
+          </div>
+
+          <div className="nav d-flex align-items-center gap-5">
+            <div className="nav__menu" ref={menuRef} onClick={menuToggle}>
+              <ul className="nav__list">
+                {navLinks.map((item, index) => (
+                  <li key={index} className="nav__item">
+                    <a href={item.url}>{item.display}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="nav__right">
+              <p className="mb-0 d-flex align-items-center gap-2">
+                <i class="ri-shopping-cart-line"></i>
+                <a href="#">Cart</a>
+              </p>
+            </div>
+          </div>
+
+          <div className="mobile__menu">
+            <span>
+              <i class="ri-menu-line" onClick={menuToggle}></i>
+            </span>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React, { Fragment } from "react";
+import Navbar from "../components/Navbar/Navbar";
+// import HeroSection from "../components/Hero-Section/HeroSection";
+// import AboutUs from "../components/About-us/AboutUs";
+// import Courses from "../components/Courses-section/Courses";
+// import ChooseUs from "../components/Choose-us/ChooseUs";
+// import Features from "../components/Feature-section/Features";
+// import FreeCourse from "../components/Free-course-section/FreeCourse";
+// import Testimonials from "../components/Testimonial/Testimonials";
+// import Footer from "../components/Footer/Footer";
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Navbar />
+      {/* <HeroSection />
+      <AboutUs />
+      <Courses />
+      <ChooseUs />
+      <Features />
+      <FreeCourse />
+      <Testimonials />
+      <Footer /> */}
+    </Fragment>
+  );
+};
+
+export default Home;
