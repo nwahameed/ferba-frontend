40ef43b82ff1f9ef760d1053e4a371d62cb7683b
nwahameed <nwahameed@gmail.com>
2023-04-07 17:30:02 +0300

    Add Free Courses Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,11 @@ import Navbar from "../components/Navbar/Navbar";
 import HomeSection from "../components/HomeSection/HeroSection";
 import AboutUs from "../components/About-us/AboutUs";
 import Courses from "../components/Courses/Courses";
-// import ChooseUs from "../components/Choose-us/ChooseUs";
-// import Features from "../components/Feature-section/Features";
-// import FreeCourse from "../components/Free-course-section/FreeCourse";
-// import Testimonials from "../components/Testimonial/Testimonials";
-// import Footer from "../components/Footer/Footer";
+import ChooseUs from "../components/Choose-us/ChooseUs";
+import Features from "../components/Feature-section/Features";
+import FreeCourses from "../components/FreeCourses/FreeCourses";
+import Testimonials from "../components/Testimonial/Testimonials";
+import Footer from "../components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -16,11 +16,11 @@ const Home = () => {
       <HomeSection />
       <AboutUs />
       <Courses />
-      {/* <ChooseUs />
+      <ChooseUs />
       <Features />
-      <FreeCourse />
+      <FreeCourses />
       <Testimonials />
-      <Footer />  */}
+      <Footer />
     </Fragment>
   );
 };
